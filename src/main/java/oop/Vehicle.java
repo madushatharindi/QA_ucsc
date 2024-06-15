d308b00284820e4235152fe2e4bf6ac8ded93973
madushatharindi <madushadesilvad6@gmail.com>
2024-06-15 14:36:54 +0530

    commit

---
+++ src/main/java/oop/Vehicle.java
@@ -0,0 +1,17 @@
+package oop;
+
+    public abstract class Vehicle{
+        protected String make;
+        protected String model;
+        protected int year;
+
+        public Vehicle(String make, String model, int year){
+            this.make = make;
+            this.model = model;
+            this.year = year;
+        }
+
+        public abstract void start();
+        public abstract void displayInfo();
+    }
+
